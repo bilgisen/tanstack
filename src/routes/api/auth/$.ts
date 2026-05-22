3f2bf1225b87da29a362422d4484099993c3040f
bilgisen <hbkarabey@gmail.com>
2026-05-22 21:29:03 +0300

    fix: rename APIRoute export to Route in API route to allow TanStack to register it

--- src/routes/api/auth/$.ts
+++ src/routes/api/auth/$.ts
@@ -1,7 +1,7 @@
 import { createAPIFileRoute } from '@tanstack/react-start/api'
 import { auth } from '../../../lib/auth'
 
-export const APIRoute = createAPIFileRoute('/api/auth/$')({
+export const Route = createAPIFileRoute('/api/auth/$')({
   GET: ({ request }) => {
     return auth.handler(request)
   },
