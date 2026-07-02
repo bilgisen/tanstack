760273fae3911109edaffc3a4dec78cb3138e871
bilgisen <bilgisen@users.noreply.github.com>
2026-07-03 02:40:56 +0300

    scr

--- src/components/ui/message-scroller.tsx
+++ src/components/ui/message-scroller.tsx
@@ -56,6 +56,7 @@ function MessageScrollerContent({
     <MessageScrollerPrimitive.Content
       data-slot="message-scroller-content"
       className={cn("flex h-max min-h-full flex-col gap-6", className)}
+      suppressHydrationWarning
       {...props}
     />
   )
@@ -71,7 +72,7 @@ function MessageScrollerItem({
       data-slot="message-scroller-item"
       scrollAnchor={scrollAnchor}
       className={cn(
-        "min-w-0 shrink-0 [contain-intrinsic-size:auto_10rem] [content-visibility:auto]",
+        "min-w-0 shrink-0",
         className
       )}
       {...props}
